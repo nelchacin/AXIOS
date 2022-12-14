4020e42c1a713d3940aaaf365a0f5ddbcc522a95
Nelson Chacin <nelchacindev@gmail.com>
2022-12-14 20:51:05 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const getCountyInfo = countryName => {
+    axios
+        .get(`https://restcountries.com/v3.1/name/${countryName}`)
+        .then(response => {
+
+            const countryDetails = response.data
+
+            countryDetails.forEach(country => {
+                let countrysBox= document.createElement('div')
+                document.querySelector("body").appendChild(countrysBox)
+                countrysBox.innerHTML =`<h2 id="country-name">${country.name.common}</h2>
+                <img src="${country.flags.png}" alt="" id="country-flag">`
+
+                country.borders.forEach(border=>{
+                    bordersList=document.createElement('ul')
+                    countrysBox.appendChild(bordersList)
+                    bordersList.innerHTML=`<li>${border}</li>`
+                })
+
+            bordersList.addEventListener('click', ()=>{
+                
+            })
+            })
+
+        
+        })
+        .catch(err => console.error(err))
+    
+}
+
+
+const countryBtn = document.querySelector('#get-country-btn')
+
+countryBtn.addEventListener('click', () => {
+    
+    const userInput = document.querySelector('#country-name-input').value
+   
+    getCountyInfo(userInput)
+
+})
